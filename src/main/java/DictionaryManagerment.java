d5e86b113c190f76d901e81f95aac818bb525fe1
QuangHaiDo <quanghaivn198@gmail.com>
2018-10-03 22:53:14 +0700

    Creat 3 classes, no new function

---
+++ src/main/java/DictionaryManagerment.java
@@ -0,0 +1,9 @@
+public class DictionaryManagerment {
+
+    public void showAllWords(){
+
+    }
+    public static void main(String[] args){
+        System.out.println("Welcome to Dictionary app!!");
+    }
+}
---
+++ src/main/java/Word.java
@@ -0,0 +1,22 @@
+public class Word {
+    // Các thuộc tính
+    private String word_target;
+    private String word_explain;
+
+    // Getter/Setter tương ứng
+    public String getWord_target() {
+        return word_target;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+}
